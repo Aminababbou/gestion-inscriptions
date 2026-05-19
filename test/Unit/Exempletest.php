044a38e6918d7ea6752795513d00630b49d80c22
Aminababbou <aminababbou2005@gmail.coom>
2026-05-19 08:49:28 +0200

    Fix: rename test to tests and update namespace

---
+++ test/Unit/Exempletest.php
@@ -0,0 +1,11 @@
+<?php
+namespace Tests\Unit;
+use PHPUnit\Framework\TestCase;
+class ExampleTest extends TestCase
+{
+ public function testAddition()
+ {
+ $this->assertEquals(2, 1 + 1);
+ }
+}
+?>
